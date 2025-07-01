0e7f73b1f636aed0683e1d7a95660d332064b9da
imved169 <vedwayangankar1@gmail.com>
2025-07-01 09:38:11 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,52 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { 
-  getDatabase, 
-  ref, 
-  set, 
-  onValue, 
-  get, 
-  child, 
-  update,
-  push,
-  onDisconnect,
-  serverTimestamp,
-  off 
-} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+import { getDatabase, ref, set, onValue, get, child } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAGU0Tsj9pwZcgwQjZzTFvGOE032l2b7HI",
   authDomain: "handicricket-d1ab7.firebaseapp.com",
-  databaseURL: "https://handicricket-d1ab7-default-rtdb.firebaseio.com",
   projectId: "handicricket-d1ab7",
   storageBucket: "handicricket-d1ab7.appspot.com",
   messagingSenderId: "356065986337",
   appId: "1:356065986337:web:399102c5dabfca2b6fe060",
-  measurementId: "G-6XS69Z6MMF"
+  measurementId: "G-6XS69Z6MMF",
+  databaseURL: "https://handicricket-d1ab7-default-rtdb.firebaseio.com/"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// Connection state monitoring
-let isOnline = true;
-const connectedRef = ref(db, ".info/connected");
-const connectionListener = onValue(connectedRef, (snap) => {
-  isOnline = snap.val() === true;
-  console.log(isOnline ? "Online" : "Offline");
-});
-
-export { 
-  db, 
-  ref, 
-  set, 
-  onValue, 
-  get, 
-  child, 
-  update,
-  push,
-  onDisconnect,
-  serverTimestamp,
-  off,
-  connectionListener,
-  isOnline 
-};
+export { db, ref, set, onValue, get, child };
