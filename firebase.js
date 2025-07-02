46ab577b2d8e8718f6b9be818afec2961ded7e37
imved169 <vedwayangankar1@gmail.com>
2025-07-02 23:30:49 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -16,9 +16,3 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 export { db, ref, set, onValue, get, child };
-
-// === Summary Push Logic ===
-export async function pushMatchSummary(room, summaryData) {
-  const summaryRef = ref(db, `rooms/${room}/summary`);
-  await set(summaryRef, summaryData);
-}
