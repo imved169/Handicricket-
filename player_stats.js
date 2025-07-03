cfbfd5c98dedf5ab9436a1d499e03b65dc0166d8
imved169 <vedwayangankar1@gmail.com>
2025-07-03 06:59:18 +0530

    Add files via upload

--- player_stats.js
+++ player_stats.js
@@ -1,28 +1,55 @@
-
 export class PlayerStats {
   constructor() {
     this.batters = {};
     this.bowlers = {};
+    this.teamStats = {};
   }
 
-  updateBatter(name, runs, isOut) {
+  updateBatter(team, name, runs, isBoundary, isOut) {
     if (!this.batters[name]) {
-      this.batters[name] = { runs: 0, balls: 0, outs: 0 };
+      this.batters[name] = { runs: 0, balls: 0, fours: 0, sixes: 0, outs: 0 };
     }
     this.batters[name].runs += runs;
     this.batters[name].balls++;
+    if (isBoundary === 4) this.batters[name].fours++;
+    if (isBoundary === 6) this.batters[name].sixes++;
     if (isOut) this.batters[name].outs++;
+
+    this.teamStats[team] = this.teamStats[team] || { runs: 0, wickets: 0 };
+    this.teamStats[team].runs += runs;
+    if (isOut) this.teamStats[team].wickets++;
+  }
+
+  updateBowler(team, name, runs, isWicket, isMaiden) {
+    if (!this.bowlers[name]) {
+      this.bowlers[name] = { runs: 0, balls: 0, wickets: 0, maidens: 0 };
+    }
+    this.bowlers[name].runs += runs;
+    this.bowlers[name].balls++;
+    if (isWicket) this.bowlers[name].wickets++;
+    if (isMaiden) this.bowlers[name].maidens++;
   }
 
   getMVP() {
-    let top = null;
-    let maxRuns = 0;
+    let mvp = null;
+    let maxPoints = 0;
+
     for (const [name, stats] of Object.entries(this.batters)) {
-      if (stats.runs > maxRuns) {
-        maxRuns = stats.runs;
-        top = name;
+      const points = stats.runs + (stats.outs * 25);
+      if (points > maxPoints) {
+        maxPoints = points;
+        mvp = name;
       }
     }
-    return top;
+
+    for (const [name, stats] of Object.entries(this.bowlers)) {
+      const points = (stats.wickets * 25) - (stats.runs / 10);
+      if (points > maxPoints) {
+        maxPoints = points;
+        mvp = name;
+      }
+    }
+
+    return mvp;
   }
 }
