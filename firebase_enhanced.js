a02e27932e979fe08a594cb80d4a38cf0d02e649
imved169 <vedwayangankar1@gmail.com>
2025-07-03 06:23:10 +0530

    Add files via upload

---
+++ firebase_enhanced.js
@@ -0,0 +1,27 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getDatabase, ref, set, onValue, get, child, push } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGU0Tsj9pwZcgwQjZzTFvGOE032l2b7HI",
+  authDomain: "handicricket-d1ab7.firebaseapp.com",
+  projectId: "handicricket-d1ab7",
+  storageBucket: "handicricket-d1ab7.appspot.com",
+  messagingSenderId: "356065986337",
+  appId: "1:356065986337:web:399102c5dabfca2b6fe060",
+  measurementId: "G-6XS69Z6MMF",
+  databaseURL: "https://handicricket-d1ab7-default-rtdb.firebaseio.com/"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export function sendReaction(roomId, reaction) {
+  const reactionsRef = ref(db, `rooms/${roomId}/reactions`);
+  return push(reactionsRef, {
+    emoji: reaction,
+    timestamp: Date.now()
+  });
+}
+
+export { db, ref, set, onValue, get, child, push };
