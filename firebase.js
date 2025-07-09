abe51b716728682581f5aeb8d866295bbad6d502
imved169 <vedwayangankar1@gmail.com>
2025-07-09 10:09:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getDatabase, ref, set, onValue, get, child } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+import { getDatabase, ref, set, onValue, get, child, update, push, remove } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAGU0Tsj9pwZcgwQjZzTFvGOE032l2b7HI",
   authDomain: "handicricket-d1ab7.firebaseapp.com",
+  databaseURL: "https://handicricket-d1ab7-default-rtdb.firebaseio.com/",
   projectId: "handicricket-d1ab7",
   storageBucket: "handicricket-d1ab7.appspot.com",
   messagingSenderId: "356065986337",
   appId: "1:356065986337:web:399102c5dabfca2b6fe060",
-  measurementId: "G-6XS69Z6MMF",
-  databaseURL: "https://handicricket-d1ab7-default-rtdb.firebaseio.com/"
+  measurementId: "G-6XS69Z6MMF"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-export { db, ref, set, onValue, get, child };
+export { db, ref, set, onValue, get, child, update, push, remove };
